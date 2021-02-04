b4530b64ca0726091b1d9618d23766eeacd32c79
Steve Yalcin <steve.yalcin@ibm.com>
2021-02-04 09:04:28 -0500

    code cleaning

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from "react";
 import loadingAnimation from "../assets/loading.gif";
 
 const Posts = () => {
-  // init the state variables to set the data that fetched from api
+  // initialise the state variables to set the data that fetched from api
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const userCallApi = async () => {
+    // consume the api url asynchronously using js fetch method
     await fetch("https://jsonplaceholder.typicode.com/posts")
       .then((response) => response.json())
       .then((data) => setData(data))
@@ -22,11 +23,11 @@ const Posts = () => {
     setTimeout(() => {
       try {
         userCallApi();
-        setLoading(false);
+        setLoading(false); // set loading state to false
       } catch (error) {
         console.log(error);
       }
-    }, 8000);
+    }, 8000); // set 8 seconds delay to show loading animation
   }, []);
 
   return (
